b53fec66861b4866d8cf08146fd85373fead14af
rik <rik@qlic.nl>
2024-10-25 15:59:39 +0200

    fix images

--- resources/app/src/app/app.routes.ts
+++ resources/app/src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 export const routes: Routes = [
     {
         path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard',
+    },
+    {
+        path: 'dashboard',
         component: DashboardComponent,
     },
 ];
--- resources/app/src/app/components/navbar/navbar.component.ts
+++ resources/app/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  private readonly router = inject(Router);
+
+  public readonly routes = [
+    { path: '/dashboard', label: 'Dashboard' },
+    { path: '/team', label: 'Team' },
+    { path: '/projects', label: 'Projects' },
+    { path: '/calendar', label: 'Calendar' },
+  ];
+  
   public isOpen = false;
 
   public toggleMobileMenu() {
     this.isOpen = !this.isOpen;
+  }
+
+  public isActive(path: string): string {
+    const isActive = this.router.isActive(path, {
+      paths: 'exact',
+      queryParams: 'ignored',
+      fragment: 'ignored',
+      matrixParams: 'ignored'
+    });
+
+    return isActive ? 'activated-route' : 'route';
   }
 }
