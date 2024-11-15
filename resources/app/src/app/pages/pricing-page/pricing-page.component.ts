5b5406cca0fa703bf78ab9c35d6affc40a5f4a18
rik <rik@qlic.nl>
2024-11-15 10:59:47 +0100

    ya

--- resources/app/src/app/pages/pricing-page/pricing-page.component.ts
+++ resources/app/src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Signal, signal, computed } from '@angular/core';
 import { SharedModule } from '../../shared.module';
 import {
   HlmCardContentDirective,
@@ -38,20 +38,50 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class PricingPageComponent {
   private readonly translate = inject(TranslateService);
+  private readonly PACKAGES_TO_SHOW = ['NJOY_1', 'NJOY_4', 'NJOY_6'];
+
   public currentTab = signal('packages');
 
-  public readonly packages: Package[] = [];
+  public packages = computed(() => {
+    const packagesData = this.translate.instant('PRICING.PACKAGES.PACKAGES');
+    const packages = Object.values(packagesData) as CapitalizedPackage[];
 
-  constructor() {
-    this.packages = this.translate.instant('PRICING.PACKAGES.PACKAGES');
-  }
+    const mappedPackages = packages.map((pkg: CapitalizedPackage) => ({
+      identifier: pkg.IDENTIFIER,
+      name: pkg.NAME,
+      price: pkg.PRICE,
+      popular: pkg.POPULAR,
+      features: pkg.FEATURES.map((feature: CapitalizedFeature) => ({
+        name: feature.NAME,
+        included: feature.INCLUDED
+      }))
+    })) as Package[];
+
+    const filteredPackages = mappedPackages.filter((pkg: Package) => this.PACKAGES_TO_SHOW.includes(pkg.identifier));
+
+    return filteredPackages;
+  });
+}
+
+export interface CapitalizedPackage {
+  IDENTIFIER: string;
+  NAME: string;
+  PRICE: string;
+  POPULAR: boolean;
+  FEATURES: CapitalizedFeature[];
 }
 
 export interface Package {
+  identifier: string;
   name: string;
   price: string;
   popular: boolean;
   features: Feature[];
+}
+
+export interface CapitalizedFeature {
+  NAME: string;
+  INCLUDED: boolean;
 }
 
 export interface Feature {
